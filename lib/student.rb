ce35eabd9431071f2e59cdd39b306abefc0f189d
Jared S <jaredjames2020@gmail.com>
2019-11-16 19:37:29 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -25,12 +25,25 @@ class Student
   
   def self.drop_table
     sql = <<-SQL 
-      DROP TABLE students (
-        id INTEGER PRIMARY KEY, 
-        name TEXT,
-        grade TEXT
-        )
+      DROP TABLE students
         SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade) 
+      VALUES (?, ?)
+    SQL
+ 
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+  
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
   end
   
 end
